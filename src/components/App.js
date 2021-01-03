7e82c20538740163df004aebe97440072dfbd0c0
saurabh-08 <66739391+saurabh-08@users.noreply.github.com>
2021-01-03 13:57:48 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,14 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
+        const relatives = ["r1", "r2", "r3"];
 
         return(
             <div id="main">
                {/* Do not remove the main div */}
+                <h3>relatives[0]</h3>
+                <h3>relatives[1]</h3>
+                <h3>relatives[2]</h3>
             </div>
         )
     }
